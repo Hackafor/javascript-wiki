d01d8f7b4b31623f38262141f4686d4993871cc2
Jonathan <jonathanelian64@gmail.com>
2023-02-14 11:02:10 -0700

    Init Javascript Wiki

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import reactLogo from './assets/react.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="flex justify-center align-middle">
+        <a href="https://vitejs.dev">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="text-3xl font-bold underline">JavaScript Wiki</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
