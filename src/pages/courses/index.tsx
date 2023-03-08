4d9de4bb4d192c718c865163fc4e7705606facd4
gustavocadev <churaalaygustavo11@gmail.com>
2023-03-07 22:09:32 -0500

    feat: add routes

---
+++ src/pages/courses/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '~/layouts/layout';
+
+export default function Courses() {
+  return (
+    <Layout title="Courses">
+      <h1>Courses</h1>
+    </Layout>
+  );
+}
---
+++ src/pages/credits/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '~/layouts/layout';
+
+export default function Credits() {
+  return (
+    <Layout title="Credits">
+      <h1>Credits</h1>
+    </Layout>
+  );
+}
