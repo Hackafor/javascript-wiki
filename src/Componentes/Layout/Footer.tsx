9b53fa7a0490f0ae9ae1af750cef18f5b03bf9f5
Daizaikun <leo-im@hotmail.com>
2023-03-08 14:11:27 -0500

    New footer

--- src/Componentes/Layout/Footer.tsx
+++ src/Componentes/Layout/Footer.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 function Footer() {
   return (
-    <footer className="dark:bg-slate-600 dark:text-white text-center p-4">
-      Footer
+    <footer className="dark:bg-slate-700 bg-gray-200 dark:text-white text-center p-2 min-w-full flex justify-center ">
+      <div className="flex justify-between container min-h-full  ">
+      <Link href={"/credits"}>Desarrolladores</Link>
+       <Link href={"https://github.com/Hackafor/javascript-wiki"} target="_blank">Github</Link>
+
+      </div>
+       
+       
     </footer>
   );
 }
