1aaa5608f33a4048b6dfdf9c31f8db55083cd2ce
Daizaikun <leo-im@hotmail.com>
2023-03-08 14:08:39 -0500

    Cambios de Margen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,16 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    screens: {
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+
+    },
+    
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
