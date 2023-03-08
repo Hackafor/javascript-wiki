d0551a7d869efb555ce7413f80ced6a4dfd57ec1
Daizaikun <leo-im@hotmail.com>
2023-03-08 14:11:45 -0500

    Fix color footer

--- src/Componentes/Layout/Footer.tsx
+++ src/Componentes/Layout/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 function Footer() {
   return (
     <footer className="dark:bg-slate-700 bg-gray-200 dark:text-white text-center p-2 min-w-full flex justify-center ">
-      <div className="flex justify-between container min-h-full  ">
-      <Link href={"/credits"}>Desarrolladores</Link>
+      <div className="flex justify-between container min-h-full text-black-important ">
+      <Link href={"/credits"} className="">Desarrolladores</Link>
        <Link href={"https://github.com/Hackafor/javascript-wiki"} target="_blank">Github</Link>
 
       </div>
