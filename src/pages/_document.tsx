192b341dd9528412fc5c41e78604c356a54c145d
gustavocadev <churaalaygustavo11@gmail.com>
2023-03-07 22:09:12 -0500

    chore: add _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
